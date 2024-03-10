af9fdf102fabcbccfe9e37c8773d8b173f9b0b46
dmzlingyin <dmzlingyin@163.com>
2024-03-10 23:01:00 +0800

    feat: add lazy util

---
+++ lazy/lazy.go
@@ -0,0 +1,30 @@
+package lazy
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type Value[T any] struct {
+	locker sync.Mutex
+	value  T
+	ok     int32
+	New    func() (T, error)
+}
+
+func (l *Value[T]) Get() (value T, err error) {
+	if atomic.LoadInt32(&l.ok) == 1 {
+		return l.value, nil
+	}
+
+	l.locker.Lock()
+	defer l.locker.Unlock()
+
+	if l.ok == 0 {
+		l.value, err = l.New()
+		if err == nil {
+			atomic.StoreInt32(&l.ok, 1)
+		}
+	}
+	return l.value, err
+}
